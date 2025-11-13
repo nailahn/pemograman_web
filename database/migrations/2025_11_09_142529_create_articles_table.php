9f990ba1c95deb93527c484226edaaf407ce3dae
Naila <nailahuwaida13@gmail.com>
2025-11-13 12:01:34 +0700

    first push

---
+++ database/migrations/2025_11_09_142529_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body'); //Json
+            $table->string('image'); //Path Gambar
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,2 @@
+<h1>halaman home</h1>
+<h4>yashhhahha</h4>
